7141fc35340875f384d8107a48b8624603c9e3b5
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-06-18 01:07:27 +0600

    top bar created

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -4,9 +4,49 @@ import "./topbar.css";
 const TopBar = () => {
 	return (
 		<div className="top">
-			<div className="topLeft">Left</div>
-			<div className="topCenter">Center</div>
-			<div className="topRight">Right</div>
+			<div className="topLeft">
+				<ul>
+					<li>
+						<a href="">
+							<i class="fa-brands fa-facebook"></i>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<i class="fa-brands fa-facebook"></i>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<i class="fa-brands fa-facebook"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="topCenter">
+				<ul>
+					<li>
+						<a href="">home</a>
+					</li>
+					<li>
+						<a href="">blogs</a>
+					</li>
+					<li>
+						<a href="">about me</a>
+					</li>
+					<li>
+						<a href="">contac</a>
+					</li>
+				</ul>
+			</div>
+			<div className="topRight">
+				<img
+					src="https://www.wallpaperflare.com/static/592/771/641/women-women-outdoors-redhead-long-hair-wallpaper.jpg"
+					alt=""
+					className="profile"
+				/>
+				<i className="searchBar fa-solid fa-magnifying-glass"></i>
+			</div>
 		</div>
 	);
 };
